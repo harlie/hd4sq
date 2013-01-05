6a1dbc65be8596a563344281bf25d3e14c5d07cb
harlie.levine@gmail.com <harlie.levine@gmail.com>
2013-01-05 18:34:17 -0500

    checkin bug

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -9,6 +9,7 @@ class Stop < ActiveRecord::Base
     options = { :body => {:v => '20130105', :oauth_token => self.itinerary.foursquare_user.access_token, :venue_id => self.venue_id, :broadcast => 'public,facebook,twitter'}}
     url = "https://api.foursquare.com/v2/checkins/add"
     reply = HTTParty.post(url, options)
+    STDERR.puts reply.body
     self.complete = true
     self.save
   end
