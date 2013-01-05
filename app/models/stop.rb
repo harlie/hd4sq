c631583a0c65f7d675f339beb6e3673fab9a309e
harlie.levine@gmail.com <harlie.levine@gmail.com>
2013-01-05 18:39:19 -0500

    checkin bug

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -2,7 +2,7 @@ class Stop < ActiveRecord::Base
   belongs_to :itinerary
   attr_accessible :complete, :itinerary_id, :name, :time_to_post, :venue_id
   
-  scope :approved_stops, joins(:itinerary).where('itineraries.approved=true')
+  scope :approved_stops, joins(:itinerary).where('itineraries.approved=true').readonly(false)
   scope :post_due, lambda { where("(complete is null OR complete !=true) AND time_to_post < ?", Time.now ) }
   
   def check_in
