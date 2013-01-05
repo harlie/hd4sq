678cc638c1e917ed4ff7dd3d1db89b71fe89fe1f
harlie.levine@gmail.com <harlie.levine@gmail.com>
2013-01-05 18:11:25 -0500

    scopes

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,4 +1,7 @@
 class Stop < ActiveRecord::Base
   belongs_to :itinerary
   attr_accessible :complete, :itinerary_id, :name, :time_to_post, :venue_id
+  
+  scope :approved_stops, joins(:itinerary).where('itineraries.approved=true')
+  scope :post_due, lambda { where("time_to_post < ?", Time.now ) }
 end
