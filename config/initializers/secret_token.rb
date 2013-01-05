6ea778c06015427afa0d80a63b81ab2f3c55c66c
harlie.levine@gmail.com <harlie.levine@gmail.com>
2013-01-05 09:32:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hd4sq::Application.config.secret_token = '8b6fbe11430f4957085d4fbf9898237089669b45100acb8d06c01d4f828d62151f24403f50132cda3658cdd2808ebc8a60ae5cad82814f7485b045be4173b002'
