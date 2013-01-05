7f721751903d5097d46f0b06daf3de5054bf0d4b
harlie.levine@gmail.com <harlie.levine@gmail.com>
2013-01-05 12:29:37 -0500

    checkin debug

---
+++ app/controllers/checkins_controller.rb
@@ -0,0 +1,27 @@
+class CheckinsController < ApplicationController
+  def checkin
+    #get category from params
+    checkin = JSON.parse(params['checkin'])
+    categories = checkin['venue']['categories']
+    home = false
+    STDERR.puts params.to_s
+    categories.each do |cat|
+      if cat['id'] == '4bf58dd8d48988d103941735'
+        home = true
+      end
+    end
+    #get checkin id
+    checkin_id = checkin['id']
+    #if home reply with you have checked in at home
+    #https://api.foursquare.com/v2/checkins/CHECKIN_ID/reply
+    if home 
+      STDERR.puts "home checkin"
+      url = "https://api.foursquare.com/v2/checkins/#{checkin_id}/reply"
+      options = { :body => {:text => 'lazy bum'}
+      reply = HTTParty.post(url, options)
+    else
+      STDERR.puts "not home checkin"
+    end
+  end
+  
+end
