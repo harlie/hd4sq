e8147635f261ca66197c81b634a6e31e51292c6e
harlie.levine@gmail.com <harlie.levine@gmail.com>
2013-01-05 21:20:08 -0500

    pretty shortcut

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -2,6 +2,16 @@ class ItinerariesController < ApplicationController
   layout "pretty"
   def show
     @itinerary = Itinerary.find_by_checkin_id(params[:id])
+    if @itinerary.demo
+      @itinerary.update_attributes(params[:itinerary])
+      if @itinerary.demo
+        @itinerary.stops.each do |stop|
+           stop.check_in
+        end
+        FollowUpMailer::follow_up(@itinerary).deliver
+      end
+      redirect_to 'http://www.couchcachet.com/screen-grab.html
+    end
   end
   
   def update
