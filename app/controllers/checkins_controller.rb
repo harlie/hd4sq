b1131fc9604a0a5d1734fb3a560d7b33fd997e75
harlie.levine@gmail.com <harlie.levine@gmail.com>
2013-01-06 14:13:46 -0500

    fixing some stuff

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -21,7 +21,7 @@ class CheckinsController < ApplicationController
       user = FoursquareUser.find_by_foursquare_id(user_id)
       itin = Itinerary.create_itinerary_from_checkin(checkin, user)
       #url = itinerary_url(itin.checkin_id)
-      url = itinerary_url(itin.to_s)
+      url = itinerary_url(itin.to_s, :protocol => "http")
       STDERR.puts url
       options = { :body => {:v => '20130105', :text => itin.to_reply, :url => url,:oauth_token => user.access_token}}
       url = "https://api.foursquare.com/v2/checkins/#{checkin_id}/reply"
