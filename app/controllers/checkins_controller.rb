373c7507e2e29df884a9ccd7252d0d53d660fd28
harlie.levine@gmail.com <harlie.levine@gmail.com>
2013-01-06 16:05:33 -0500

    cleaning up some debug code we don't need anymore

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -5,7 +5,6 @@ class CheckinsController < ApplicationController
     checkin = JSON.parse(params['checkin'])
     categories = checkin['venue']['categories']
     home = false
-    STDERR.puts params.to_s
     categories.each do |cat|
       if cat['id'] == '4bf58dd8d48988d103941735'
         home = true
@@ -16,19 +15,15 @@ class CheckinsController < ApplicationController
     #if home reply with you have checked in at home
     #https://api.foursquare.com/v2/checkins/CHECKIN_ID/reply
     if home 
-      STDERR.puts "home checkin"
       user_id = checkin['user']['id']
       user = FoursquareUser.find_by_foursquare_id(user_id)
       itin = Itinerary.create_itinerary_from_checkin(checkin, user)
       #url = itinerary_url(itin.checkin_id)
       url = itinerary_url(itin.to_s, :protocol => "http")
-      STDERR.puts url
       options = { :body => {:v => '20130105', :text => itin.to_reply, :url => url,:oauth_token => user.access_token}}
       url = "https://api.foursquare.com/v2/checkins/#{checkin_id}/reply"
       reply = HTTParty.post(url, options)
-      STDERR.puts reply.to_s
     else
-      STDERR.puts "not home checkin"
     end
     render text: 'OK'
   end
