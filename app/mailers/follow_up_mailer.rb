70ccff20109946ef7646ee3c4405b338aa3a6d9c
harlie.levine@gmail.com <harlie.levine@gmail.com>
2013-01-06 09:51:19 -0500

    fixing date formats in emails

--- app/mailers/follow_up_mailer.rb
+++ app/mailers/follow_up_mailer.rb
@@ -6,14 +6,13 @@ class FollowUpMailer < ActionMailer::Base
     @itinerary.zip =  @itinerary.zip ?  @itinerary.zip : 10001
     start_date = Time.now.strftime('%m/%d/%y')
     end_date = (Time.now + 1.week).strftime('%m/%d/%y')
-    url = "http://api.jambase.com/search?apikey=78hjynre9at8tk4grtq3fdz7&zip=#{@itinerary.zip}&radius=10&n=10&startDate=#{start_date}&endDate=#{end_date}"
-    STDERR.puts url
+    url = "http://api.jambase.com/search?apikey=78hjynre9at8tk4grtq3fdz7&zip=#{@itinerary.zip}&radius=10&n=10&user=jambase&startDate=#{start_date}&endDate=#{end_date}"
     xml_data = Net::HTTP.get_response(URI.parse(url)).body
     doc = XmlSimple.xml_in(xml_data, { 'KeyAttr' => 'name' })    
     @shows = Array.new
     doc['event'].each do |event|
       venue_name = event['venue'][0]['venue_name'][0]
-      date = event['event_date'][0]
+      date = event['event_date'][0].gsub(/(\d*\/\d*)\/\d*/, '\1')
       artist = event['artists'][0]['artist'][0]['artist_name'][0]
       event_url = event['event_url'][0]
       @shows << { :title => "#{artist} @ #{venue_name}", :date => date , :url => event_url}
@@ -24,7 +23,7 @@ class FollowUpMailer < ActionMailer::Base
     @activities = Array.new
     active = JSON.parse(json_data)
     active['_results'].each do |res|
-      date = res['meta']['startDate'].gsub(/\d*-(\d*)-(\d*)/, '\1/\2')
+      date = res['meta']['startDate'].gsub(/\d*-0*(\d*)-0*(\d*)/, '\1/\2')
       @activities << { :title => res['title'], :date => date , :url => res['url']}
     end
     mail to: @itinerary.foursquare_user.get_email, subject: "Get off your couch"
