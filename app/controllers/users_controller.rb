1798bc539baca62d33ec9c287941d9a8ab177345
Brian Fountain <brian@brianfountain.com>
2013-01-06 19:04:37 -0500

    Created template for main index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+  layout "users"
   def new
-    render text: "<a href='#{new_foursquare_client_path}'><img src='https://playfoursquare.s3.amazonaws.com/press/logo/connect-black.png' /></a>"
+    # render text: "<a href='#{new_foursquare_client_path}'><img src='https://playfoursquare.s3.amazonaws.com/press/logo/connect-black.png' /></a>"
   end
 
   def show
-    render text: "hello #{current_user.name}.  Welcome to CouchCachet.  Next time you check in from home, we'll be in touch."
+    @user = current_user.name
+    # render text: "hello #{current_user.name}.  Welcome to CouchCachet.  Next time you check in from home, we'll be in touch."
   end
 
   private
